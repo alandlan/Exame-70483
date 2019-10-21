6c043892e2d3bba64ec02186a0818af5e1054a68
Alan Martins <alanhorcrux@gmail.com>
2019-10-21 20:15:19 -0300

    Criado novas classes

---
+++ Exame-70483/AplicativoConsole/Tipos/Conta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicativoConsole.Tipos
+{
+    public class Conta
+    {
+        public int Id { get; set; }
+        public Enum TipoConta { get; set; }
+        public int Cliente { get; set; }
+        public decimal Saldo { get; set; }
+    }
+}
